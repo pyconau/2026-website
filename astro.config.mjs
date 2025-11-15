bedb13c3bc5ce101c56ef6e6af282e1db39ef69f
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-11-15 18:36:26 +1100

    Migrate to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://2026.pycon.org.au',
+  site: "https://2026.pycon.org.au",
   markdown: {
     syntaxHighlight: "prism",
   },
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false, // We'll use our own base styles
-    }),
-  ],
+  integrations: [mdx()],
   redirects: {
     // Add any necessary redirects here
   },
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
